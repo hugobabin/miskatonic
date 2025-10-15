d574e500268c56278ea52c2642acbbac025f95c7
Hugo Babin <hugobabin.contact@gmail.com>
2025-10-15 09:40:53 +0200

    archive questions/quizs

--- src/routers/quiz.py
+++ src/routers/quiz.py
@@ -1,4 +1,3 @@
-# ...existing code...
 from fastapi import APIRouter, Request
 from fastapi.responses import ORJSONResponse
 
@@ -13,7 +12,6 @@ router = APIRouter(
 
 @router.get("/", tags=["quizs"], name="quizs")
 async def get_quizs(request: Request) -> ORJSONResponse:
-    # REFACTORING MVT // Retourne les quizs en JSONue
     quizs = ServiceQuiz.list_all()
     quizs = [quiz.model_dump() for quiz in quizs]
     return handle_request_success(request=request, data={"quizs": quizs})
@@ -21,7 +19,6 @@ async def get_quizs(request: Request) -> ORJSONResponse:
 
 @router.post("/generate", tags=["quizs"])
 async def generate_quiz(params: QuizGenerator, request: Request) -> ORJSONResponse:
-    # REFACTORING MVT // Génération via JSON, renvoie JSON
     ServiceQuiz.generate(
         total_questions=params.total_questions,
         subjects=params.subjects,
@@ -35,12 +32,8 @@ async def generate_quiz(params: QuizGenerator, request: Request) -> ORJSONRespon
 
 @router.post("/{quiz}/archive", tags=["quizs"])
 async def archive_quiz(quiz: str, request: Request) -> ORJSONResponse:
-    # REFACTORING MVT // Archive et renvoie JSON
     ServiceQuiz.archive(quiz_id=quiz)
     return handle_request_success(
         request=request,
         data={"success": True, "message": f"Successfully archived quiz with id {quiz}"},
     )
-
-
-# ...existing code...
